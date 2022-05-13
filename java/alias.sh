c15333d2f901c131c3c71e0efaefec31f38b7e01
Emre Arslan <ce.earslan@gmail.com>
2022-05-13 08:33:52 +0300

    added java

---
+++ java/alias.sh
@@ -0,0 +1,12 @@
+# Current SDK
+alias currentjdk="sdk current java"
+
+# Default JDK version
+alias jdkdefault8="sdk default java 8.0.322-zulu"
+alias jdkdefault11="sdk default java 11.0.14-zulu"
+alias jdkdefault12="sdk default java 12.0.2.j9-adpt"
+alias jdkdefault12hs="sdk default java 12.0.2.hs-adpt"
+
+# Maven
+alias mci='mvn clean install'
+alias mciwot='mvn clean install -DskipTests'
---
+++ java/install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils/setup.sh"
+
+declare -r SDKMAN_INIT_FILE="$HOME/.dotfiles/java/init.sh"
+declare -r SDKMAN_DIRECTORY="$HOME/.sdkman"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+is_sdkman_installed() {
+    [ -d "$SDKMAN_DIRECTORY" ]
+}
+
+install_sdkman() {
+    
+    printf "\n" | /bin/bash -c "$(curl -s "https://get.sdkman.io")" &> /dev/null
+    #  └─ simulate the ENTER keypress
+
+    print_result $? "Sdkman (install)"
+    
+}
+
+update_sdkman() {
+
+    execute \
+        ". $SDKMAN_INIT_FILE \
+            && sdk update" \
+        "Sdkman (upgrade)"
+
+}
+
+sdk_install() {
+
+    declare -r SDK_NAME="$1"
+    declare -r SDK="$2"
+    declare -r IDENTIFIER="$3"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Check if `Sdkman` is installed.
+
+    if ! is_sdkman_installed; then
+        print_error "$SDK_NAME ('Sdkman' is not installed)"
+        return 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Install the specific version by Identifier.
+
+    execute \
+        ". $SDKMAN_INIT_FILE \
+            && sdk install $SDK $IDENTIFIER" \
+        "$SDK_NAME"
+
+}
+
+main() {
+
+    print_in_purple "\n   Java\n\n"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if ! is_sdkman_installed; then
+        install_sdkman
+    else
+        update_sdkman
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    sdk_install "Java 8 - Zulu" "java" "8.0.322-zulu"
+    sdk_install "Java 11 - Zulu" "java" "11.0.14-zulu"
+    sdk_install "Java 17 - Temurin" "java" "17.0.2-tem"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    sdk_install "Maven (install latest)" "maven"
+    
+}
+
+main
