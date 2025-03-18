53a83331d51ed8693dd3c9bd9db96f5a2cfe5198
Emre Arslan <ce.earslan@gmail.com>
2025-03-18 06:26:52 +0300

    update java 8 jdk version

--- java/alias.sh
+++ java/alias.sh
@@ -2,7 +2,7 @@
 alias currentjdk="sdk current java"
 
 # Default JDK version
-alias jdkdefault8="sdk default java 8.0.322-zulu"
+alias jdkdefault8="sdk default java 8.0.442-zulu"
 alias jdkdefault11="sdk default java 11.0.24-zulu"
 alias jdkdefault12="sdk default java 12.0.2.j9-adpt"
 alias jdkdefault12hs="sdk default java 12.0.2.hs-adpt"
